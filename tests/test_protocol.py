7268b09f82d820402a7f7a6e8733bb402510aaad
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-02-29 17:42:19 +0100

    tests

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -6,36 +6,59 @@ images_dir_path = os.path.abspath("./tests/data/images")
 # def test_create_visualization(server):
 #     server.call("create_visualization")
 #     abs_path = os.path.join(images_dir_path, "create_visualization.jpeg")
-#     assert server.compare_image(4, abs_path) == True
+#     assert server.compare_image(3, abs_path) == True
 
 
 # def test_reset_camera(server):
 #     server.call("reset_camera")
 #     abs_path = os.path.join(images_dir_path, "reset_camera.jpeg")
-#     assert server.compare_image(4, abs_path) == True
+#     assert server.compare_image(3, abs_path) == True
 
 
 # def test_create_object_pipeline(server):
 #     server.call("create_object_pipeline", [{"id": "123456", "file_name": "hat.vtp"}])
 #     abs_path = os.path.join(images_dir_path, "create_object_pipeline.jpeg")
-#     assert server.compare_image(4, abs_path) == True
+#     assert server.compare_image(3, abs_path) == True
 
 
-def test_delete_object_pipeline(server):
-    server.call("create_object_pipeline", [{"id": "123456789", "file_name": "hat.vtp"}])
-    abs_path = os.path.join(images_dir_path, "create_object_pipeline.jpeg")
-    assert server.compare_image(4, abs_path) == True
+# def test_delete_object_pipeline(server):
+#     server.call("create_object_pipeline", [{"id": "123456789", "file_name": "hat.vtp"}])
+#     abs_path = os.path.join(images_dir_path, "create_object_pipeline.jpeg")
+#     assert server.compare_image(3, abs_path) == True
 
-    server.call("delete_object_pipeline", [{"id": "123456789"}])
-    abs_path = os.path.join(images_dir_path, "delete_object_pipeline.jpeg")
-    assert server.compare_image(4, abs_path) == True
+#     server.call("delete_object_pipeline", [{"id": "123456789"}])
+#     abs_path = os.path.join(images_dir_path, "delete_object_pipeline.jpeg")
+#     assert server.compare_image(3, abs_path) == True
 
 
 # def test_toggle_object_visibility(server):
 #     server.call("create_object_pipeline", [{"id": "123456789", "file_name": "hat.vtp"}])
 #     abs_path = os.path.join(images_dir_path, "create_object_pipeline.jpeg")
-#     assert server.compare_image(4, abs_path) == True
+#     assert server.compare_image(3, abs_path) == True
 
 #     server.call("toggle_object_visibility", [{"id": "123456789", "is_visible": False}])
-#     abs_path = os.path.join(images_dir_path, "toggle_object_visibility.jpeg")
+#     abs_path = os.path.join(images_dir_path, "toggle_object_visibility_1.jpeg")
 #     assert server.compare_image(3, abs_path) == True
+
+#     server.call("toggle_object_visibility", [{"id": "123456789", "is_visible": True}])
+#     abs_path = os.path.join(images_dir_path, "toggle_object_visibility_2.jpeg")
+#     assert server.compare_image(3, abs_path) == True
+
+
+def test_apply_textures(server):
+
+    server.call("create_object_pipeline", [{"id": "123456789", "file_name": "hat.vtp"}])
+    abs_path = os.path.join(images_dir_path, "create_object_pipeline.jpeg")
+    assert server.compare_image(3, abs_path) == True
+
+    server.call(
+        "apply_textures",
+        [
+            {
+                "id": "123456789",
+                "textures": {"texture_name": "lambert2SG", "texture_file_name": ""},
+            }
+        ],
+    )
+    abs_path = os.path.join(images_dir_path, "apply_textures.jpeg")
+    assert server.compare_image(1, abs_path) == True
