2e68158e19c8b8023b34c49f3fa5a482164c3cf5
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-01-03 16:44:53 +0100

    fix(colors): r/g/b between 0 & 1

--- src/opengeodeweb_viewer/rpc/mesh/edges/edges_protocols.py
+++ src/opengeodeweb_viewer/rpc/mesh/edges/edges_protocols.py
@@ -28,7 +28,7 @@ class VtkMeshEdgesView(VtkMeshView):
         print(self.mesh_edges_prefix + self.mesh_edges_schemas_dict["color"]["rpc"], f"{params=}", flush=True)
         validate_schema(params, self.mesh_edges_schemas_dict["color"])
         id = params["id"]
-        red, green, blue = params["color"]["r"], params["color"]["g"], params["color"]["b"]
+        red, green, blue = params["color"]["r"]/255, params["color"]["g"]/255, params["color"]["b"]/255
         self.SetEdgesColor(id, [red, green, blue])
 
     @exportRpc(mesh_edges_prefix + mesh_edges_schemas_dict["size"]["rpc"])
