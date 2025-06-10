aca092c3af7b4749b8333035ead065f94f5aa309
SpliiT <spliitdev@gmail.com>
2025-06-10 15:30:42 +0200

    fix(schemas): Fix Edges & Points schemas

--- src/opengeodeweb_viewer/rpc/model/model_protocols.py
+++ src/opengeodeweb_viewer/rpc/model/model_protocols.py
@@ -51,27 +51,3 @@ class VtkModelView(VtkObjectView):
         )
         id, visibility = params["id"], params["visibility"]
         self.SetVisibility(id, visibility)
-
-    @exportRpc(model_prefix + model_schemas_dict["points.visibility"]["rpc"])
-    def setModelPointsVisibility(self, params):
-        validate_schema(
-            params, self.model_schemas_dict["points.visibility"], self.model_prefix
-        )
-        id, visibility = params["id"], params["visibility"]
-        self.SetPointsVisibility(id, visibility)
-
-    @exportRpc(model_prefix + model_schemas_dict["points.size"]["rpc"])
-    def setModelPointsSize(self, params):
-        validate_schema(
-            params, self.model_schemas_dict["points.size"], self.model_prefix
-        )
-        id, size = params["id"], params["size"]
-        self.SetPointsSize(id, size)
-
-    @exportRpc(model_prefix + model_schemas_dict["edges.visibility"]["rpc"])
-    def setModelEdgesVisibility(self, params):
-        validate_schema(
-            params, self.model_schemas_dict["edges.visibility"], self.model_prefix
-        )
-        id, visibility = params["id"], params["visibility"]
-        self.SetEdgesVisibility(id, visibility)
---
+++ src/tests/model/points/test_model_points_protocols.py
@@ -0,0 +1,33 @@
+# Standard library imports
+
+# Third party imports
+from opengeodeweb_viewer.rpc.model.points.points_protocols import (
+    VtkModelPointsView,
+)
+
+# Local application imports
+from src.tests.model.test_model_protocols import test_register_model
+
+
+def test_points_visibility(server):
+
+    test_register_model(server)
+
+    server.call(
+        VtkModelPointsView.model_points_prefix
+        + VtkModelPointsView.model_points_schemas_dict["visibility"]["rpc"],
+        [{"id": "123456789", "visibility": True}],
+    )
+    assert server.compare_image(3, "model/points/visibility.jpeg") == True
+
+
+def test_points_size(server):
+
+    test_points_visibility(server)
+
+    server.call(
+        VtkModelPointsView.model_points_prefix
+        + VtkModelPointsView.model_points_schemas_dict["size"]["rpc"],
+        [{"id": "123456789", "size": 20}],
+    )
+    assert server.compare_image(3, "model/points/size.jpeg") == True
