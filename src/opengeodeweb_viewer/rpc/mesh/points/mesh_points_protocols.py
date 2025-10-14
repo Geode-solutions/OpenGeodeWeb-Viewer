bc6c54fe8be8554d4b7b26bc43021ea3c0c17947
MaxNumerique <maximilienlandois@gmail.com>
2025-10-14 10:00:23 +0200

    revert

--- src/opengeodeweb_viewer/rpc/mesh/points/mesh_points_protocols.py
+++ src/opengeodeweb_viewer/rpc/mesh/points/mesh_points_protocols.py
@@ -11,7 +11,6 @@ from opengeodeweb_viewer.utils_functions import (
     validate_schema,
     RpcParams,
     RpcParamsWithColor,
-    JsonValue,
 )
 from opengeodeweb_viewer.rpc.mesh.mesh_protocols import VtkMeshView
 
@@ -25,24 +24,18 @@ class VtkMeshPointsView(VtkMeshView):
     def __init__(self) -> None:
         super().__init__()
 
-    @exportRpc(
-        mesh_points_prefix + cast(str, mesh_points_schemas_dict["visibility"]["rpc"])
-    )
+    @exportRpc(mesh_points_prefix + mesh_points_schemas_dict["visibility"]["rpc"])
     def setMeshPointsVisibility(self, params: RpcParams) -> None:
         validate_schema(
-            params,
-            cast(dict[str, JsonValue], self.mesh_points_schemas_dict["visibility"]),
-            self.mesh_points_prefix,
+            params, self.mesh_points_schemas_dict["visibility"], self.mesh_points_prefix
         )
         id, visibility = params["id"], params["visibility"]
         self.SetPointsVisibility(id, visibility)
 
-    @exportRpc(mesh_points_prefix + cast(str, mesh_points_schemas_dict["color"]["rpc"]))
+    @exportRpc(mesh_points_prefix + mesh_points_schemas_dict["color"]["rpc"])
     def setMeshPointsColor(self, params: RpcParamsWithColor) -> None:
         validate_schema(
-            params,
-            cast(dict[str, JsonValue], self.mesh_points_schemas_dict["color"]),
-            self.mesh_points_prefix,
+            params, self.mesh_points_schemas_dict["color"], self.mesh_points_prefix
         )
         id, red, green, blue = (
             params["id"],
@@ -52,26 +45,19 @@ class VtkMeshPointsView(VtkMeshView):
         )
         self.SetPointsColor(id, red, green, blue)
 
-    @exportRpc(mesh_points_prefix + cast(str, mesh_points_schemas_dict["size"]["rpc"]))
+    @exportRpc(mesh_points_prefix + mesh_points_schemas_dict["size"]["rpc"])
     def setMeshPointsSize(self, params: RpcParams) -> None:
         validate_schema(
-            params,
-            cast(dict[str, JsonValue], self.mesh_points_schemas_dict["size"]),
-            self.mesh_points_prefix,
+            params, self.mesh_points_schemas_dict["size"], self.mesh_points_prefix
         )
         id, size = params["id"], params["size"]
         self.SetPointsSize(id, size)
 
-    @exportRpc(
-        mesh_points_prefix
-        + cast(str, mesh_points_schemas_dict["vertex_attribute"]["rpc"])
-    )
+    @exportRpc(mesh_points_prefix + mesh_points_schemas_dict["vertex_attribute"]["rpc"])
     def setMeshPointsVertexAttribute(self, params: RpcParams) -> None:
         validate_schema(
             params,
-            cast(
-                dict[str, JsonValue], self.mesh_points_schemas_dict["vertex_attribute"]
-            ),
+            self.mesh_points_schemas_dict["vertex_attribute"],
             self.mesh_points_prefix,
         )
         id, name = params["id"], params["name"]
--- src/tests/mesh/points/test_mesh_points_protocols.py
+++ src/tests/mesh/points/test_mesh_points_protocols.py
@@ -1,5 +1,5 @@
 # Standard library imports
-from typing import Callable, cast
+from typing import Callable
 
 # Third party imports
 from src.opengeodeweb_viewer.rpc.mesh.mesh_protocols import VtkMeshView
@@ -20,7 +20,7 @@ def test_points_visibility(
 
     server.call(
         VtkMeshPointsView.mesh_points_prefix
-        + cast(str, VtkMeshPointsView.mesh_points_schemas_dict["visibility"]["rpc"]),
+        + VtkMeshPointsView.mesh_points_schemas_dict["visibility"]["rpc"],
         [{"id": mesh_id, "visibility": True}],
     )
     assert server.compare_image(3, "mesh/points/visibility.jpeg") == True
@@ -34,7 +34,7 @@ def test_points_size(
 
     server.call(
         VtkMeshPointsView.mesh_points_prefix
-        + cast(str, VtkMeshPointsView.mesh_points_schemas_dict["size"]["rpc"]),
+        + VtkMeshPointsView.mesh_points_schemas_dict["size"]["rpc"],
         [{"id": mesh_id, "size": 15}],
     )
     assert server.compare_image(3, "mesh/points/size.jpeg") == True
@@ -48,7 +48,7 @@ def test_points_color(
 
     server.call(
         VtkMeshPointsView.mesh_points_prefix
-        + cast(str, VtkMeshPointsView.mesh_points_schemas_dict["color"]["rpc"]),
+        + VtkMeshPointsView.mesh_points_schemas_dict["color"]["rpc"],
         [{"id": mesh_id, "color": {"r": 255, "g": 0, "b": 0}}],
     )
     assert server.compare_image(3, "mesh/points/color.jpeg") == True
@@ -61,29 +61,28 @@ def test_points_with_point_set(
 
     dataset_factory(id=mesh_id, viewable_file_name="points.vtp")
     server.call(
-        VtkMeshView.mesh_prefix
-        + cast(str, VtkMeshView.mesh_schemas_dict["register"]["rpc"]),
+        VtkMeshView.mesh_prefix + VtkMeshView.mesh_schemas_dict["register"]["rpc"],
         [{"id": mesh_id}],
     )
     assert server.compare_image(3, "mesh/points/register_point_set.jpeg") == True
 
     server.call(
         VtkMeshPointsView.mesh_points_prefix
-        + cast(str, VtkMeshPointsView.mesh_points_schemas_dict["size"]["rpc"]),
+        + VtkMeshPointsView.mesh_points_schemas_dict["size"]["rpc"],
         [{"id": mesh_id, "size": 10}],
     )
     assert server.compare_image(3, "mesh/points/point_set_size.jpeg") == True
 
     server.call(
         VtkMeshPointsView.mesh_points_prefix
-        + cast(str, VtkMeshPointsView.mesh_points_schemas_dict["color"]["rpc"]),
+        + VtkMeshPointsView.mesh_points_schemas_dict["color"]["rpc"],
         [{"id": mesh_id, "color": {"r": 255, "g": 0, "b": 0}}],
     )
     assert server.compare_image(3, "mesh/points/point_set_color.jpeg") == True
 
     server.call(
         VtkMeshPointsView.mesh_points_prefix
-        + cast(str, VtkMeshPointsView.mesh_points_schemas_dict["visibility"]["rpc"]),
+        + VtkMeshPointsView.mesh_points_schemas_dict["visibility"]["rpc"],
         [{"id": mesh_id, "visibility": False}],
     )
     assert server.compare_image(3, "mesh/points/point_set_visibility.jpeg") == True
