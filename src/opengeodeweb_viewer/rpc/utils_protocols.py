92fd9bb095f8751f09c0c4137e6c280a5b8e82c7
Arnaud Botella <arnaud.botella@geode-solutions.com>
2025-10-22 15:39:33 +0200

    test

--- src/opengeodeweb_viewer/rpc/utils_protocols.py
+++ src/opengeodeweb_viewer/rpc/utils_protocols.py
@@ -1,9 +1,8 @@
-# type: ignore
 # Standard library imports
 import os
 
 # Third party imports
-from wslink import register as exportRpc
+from wslink import register as exportRpc  # type: ignore
 
 # Local application imports
 from opengeodeweb_microservice.schemas import get_schemas_dict
