359eb6faec3529d328289ca83dde7e349679947e
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-10-27 12:45:10 +0100

    test os._exit(0)

--- src/opengeodeweb_viewer/rpc/utils_protocols.py
+++ src/opengeodeweb_viewer/rpc/utils_protocols.py
@@ -1,6 +1,26 @@
 # Standard library imports
 import os
-import time
+
+# Third party imports
+from wslink import register as exportRpc  # type: ignore
+
+# Local application imports
+from opengeodeweb_microservice.schemas import get_schemas_dict
+from opengeodeweb_viewer.vtk_protocol import VtkView
+
+
+class VtkUtilsView(VtkView):
+    utils_prefix = "opengeodeweb_viewer."
+    utils_schemas_dict = get_schemas_dict(
+        os.path.join(os.path.dirname(__file__), "schemas")
+    )
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    @exportRpc(utils_prefix + utils_schemas_dict["kill"]["rpc"])
+# Standard library imports
+import os
 
 # Third party imports
 from wslink import register as exportRpc  # type: ignore
@@ -21,7 +41,6 @@ class VtkUtilsView(VtkView):
 
     @exportRpc(utils_prefix + utils_schemas_dict["kill"]["rpc"])
     def kill(self) -> None:
+        print(f"{self.utils_prefix + self.utils_schemas_dict['kill']['rpc']}", flush=True)
         print("Manual viewer kill, shutting down...", flush=True)
-        self.publish("kill", "Viewer killed")
-        time.sleep(5)
         os._exit(0)
