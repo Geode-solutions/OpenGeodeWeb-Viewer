1758f43f2bcd700b843be7416a7d8ce5864d7c1c
MaxNumerique <maximilienlandois@gmail.com>
2025-10-29 17:02:24 +0100

    feat(import_project): new schema, rpc added to reset viewer and prepare imported datas

--- src/opengeodeweb_viewer/rpc/utils_protocols.py
+++ src/opengeodeweb_viewer/rpc/utils_protocols.py
@@ -7,6 +7,7 @@ from wslink import register as exportRpc  # type: ignore
 # Local application imports
 from opengeodeweb_microservice.schemas import get_schemas_dict
 from opengeodeweb_viewer.vtk_protocol import VtkView
+from opengeodeweb_microservice.database import connection
 
 
 class VtkUtilsView(VtkView):
@@ -25,3 +26,29 @@ class VtkUtilsView(VtkView):
         )
         print("Manual viewer kill, shutting down...", flush=True)
         os._exit(0)
+
+    @exportRpc(utils_prefix + utils_schemas_dict["reset_project"]["rpc"])
+    def resetProject(self, rpc_params: dict | None = None) -> None:
+        print(
+            f"{self.utils_prefix + self.utils_schemas_dict['reset_project']['rpc']}",
+            flush=True,
+        )
+        renderWindow = self.getView("-1")
+        renderer = renderWindow.GetRenderers().GetFirstRenderer()
+
+        renderer.RemoveAllViewProps()
+
+        widget = self.get_widget()
+        if widget is not None:
+            try:
+                widget.EnabledOff()
+            except Exception:
+                pass
+        self.coreServer.setSharedObject("widget", None)
+        self.coreServer.setSharedObject("grid_scale", None)
+        self.coreServer.setSharedObject("axes", None)
+
+        self.get_data_base().clear()
+
+        db_full_path = os.path.join(self.DATA_FOLDER_PATH, "project.db")
+        connection.init_database(db_full_path, create_tables=False)
