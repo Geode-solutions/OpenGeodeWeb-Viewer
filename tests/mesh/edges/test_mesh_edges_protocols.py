b97834b8c7e1cbe8c96f237a060e2ec9eb49d088
MaxNumerique <maximilienlandois@gmail.com>
2026-01-08 12:05:25 +0100

    adding attributed_edged_curve data and adapt tests

--- tests/mesh/edges/test_mesh_edges_protocols.py
+++ tests/mesh/edges/test_mesh_edges_protocols.py
@@ -66,12 +66,17 @@ def test_edges_vertex_attribute(
     server: ServerMonitor, dataset_factory: Callable[..., str]
 ) -> None:
     mesh_id = "123456789"
-    test_register_mesh(server, dataset_factory)
+    dataset_factory(id=mesh_id, viewable_file="attributed_edged_curve.vtp")
+
+    server.call(
+        VtkMeshView.mesh_prefix + VtkMeshView.mesh_schemas_dict["register"]["rpc"],
+        [{"id": mesh_id}],
+    )
 
     server.call(
         VtkMeshEdgesView.mesh_edges_prefix
         + VtkMeshEdgesView.mesh_edges_schemas_dict["vertex_attribute"]["rpc"],
-        [{"id": mesh_id, "name": "lambert2SG"}],
+        [{"id": mesh_id, "name": "vertex_attribute"}],
     )
     assert server.compare_image("mesh/edges/vertex_attribute.jpeg") == True
 
