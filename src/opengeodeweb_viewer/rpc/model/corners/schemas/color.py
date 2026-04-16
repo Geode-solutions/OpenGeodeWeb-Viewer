a145210a1c4817304c2276d35a135556e837fb29
MaxNumerique <144453705+MaxNumerique@users.noreply.github.com>
2026-04-16 13:32:12 +0000

    Apply prepare changes

--- src/opengeodeweb_viewer/rpc/model/corners/schemas/color.py
+++ src/opengeodeweb_viewer/rpc/model/corners/schemas/color.py
@@ -1,5 +1,31 @@
-from opengeodeweb_viewer.rpc.model.schemas.color import (
-    Color as Color,
-    ColorClass as ColorClass,
-    ColorMode as ColorMode,
-)
+from dataclasses_json import DataClassJsonMixin
+from dataclasses import dataclass
+from typing import Optional, List
+from enum import Enum
+
+
+@dataclass
+class ColorClass(DataClassJsonMixin):
+    def __post_init__(self) -> None:
+        print(self, flush=True)
+
+    b: int
+    g: int
+    r: int
+    a: Optional[float] = None
+
+
+class ColorMode(Enum):
+    CONSTANT = "constant"
+    RANDOM = "random"
+
+
+@dataclass
+class Color(DataClassJsonMixin):
+    def __post_init__(self) -> None:
+        print(self, flush=True)
+
+    block_ids: List[int]
+    color_mode: ColorMode
+    id: str
+    color: Optional[ColorClass] = None
