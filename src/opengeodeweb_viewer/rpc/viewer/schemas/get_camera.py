39c3d778843b05dcf0371604218e569e6ef22b18
SpliiT <106495600+SpliiT@users.noreply.github.com>
2026-04-30 11:34:46 +0000

    Apply prepare changes

--- src/opengeodeweb_viewer/rpc/viewer/schemas/get_camera.py
+++ src/opengeodeweb_viewer/rpc/viewer/schemas/get_camera.py
@@ -1,8 +1,1 @@
 from dataclasses_json import DataClassJsonMixin
-from dataclasses import dataclass
-
-
-@dataclass
-class GetCamera(DataClassJsonMixin):
-    def __post_init__(self) -> None:
-        print(self, flush=True)
