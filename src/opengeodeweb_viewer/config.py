9fb872be4049e30ee9fae9b9104926692dbdbbd7
JulienChampagnol <91873154+JulienChampagnol@users.noreply.github.com>
2026-06-29 11:40:11 +0000

    Apply prepare changes

--- src/opengeodeweb_viewer/config.py
+++ src/opengeodeweb_viewer/config.py
@@ -7,7 +7,9 @@ def default_config() -> None:
     os.environ["HOST"] = "localhost"
     os.environ["PORT"] = "1234"
     os.environ["PROJECT_FOLDER_PATH"] = "/project/"
-    os.environ["DATA_FOLDER_PATH"] = os.path.join(os.environ["PROJECT_FOLDER_PATH"], "data")
+    os.environ["DATA_FOLDER_PATH"] = os.path.join(
+        os.environ["PROJECT_FOLDER_PATH"], "data"
+    )
 
 
 def prod_config() -> None:
