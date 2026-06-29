ff3f67de33a03d329180acf8a1341d6dd6e98469
JulienChampagnol <julien.champagnol@geode-solutions.com>
2026-06-29 11:38:50 +0200

    fix tests

--- src/opengeodeweb_viewer/config.py
+++ src/opengeodeweb_viewer/config.py
@@ -6,12 +6,12 @@ from sys import platform
 def default_config() -> None:
     os.environ["HOST"] = "localhost"
     os.environ["PORT"] = "1234"
+    os.environ["PROJECT_FOLDER_PATH"] = "/project/"
     os.environ["DATA_FOLDER_PATH"] = os.path.join(os.environ["PROJECT_FOLDER_PATH"], "data")
 
 
 def prod_config() -> None:
     default_config()
-    os.environ["PROJECT_FOLDER_PATH"] = "/project/"
 
 def dev_config() -> None:
     default_config()
