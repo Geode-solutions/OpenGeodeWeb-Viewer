ce71c154bf643ec66edc13720ef50e4626934916
MaxNumerique <maximilienlandois@gmail.com>
2026-08-03 10:52:38 +0200

    fix(colormap): Increase scalar bar label precision

--- src/opengeodeweb_viewer/vtk_protocol.py
+++ src/opengeodeweb_viewer/vtk_protocol.py
@@ -456,7 +456,7 @@ class VtkView(VtkTypingMixin, vtk_protocols.vtkWebProtocol):
                 bar_height = 0.08
 
             bar.SetNumberOfLabels(2)
-            bar.SetLabelFormat("%.2g")
+            bar.SetLabelFormat("%.4g")
             bar.SetOrientationToHorizontal()
 
             row = i // cols
